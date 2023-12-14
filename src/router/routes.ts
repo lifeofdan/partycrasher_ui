491981cd17b3ca62279349649caab5e54fe711b9
Daniel Newman <contact@lifeofdan.me>
2023-12-15 00:15:27 +1000

    Add routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,7 +4,11 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }]
+    children: [
+      { path: '', component: () => import('pages/LoginPage.vue') },
+      { path: 'queue', component: () => import('pages/QueuePage.vue') },
+      { path: 'playlists', component: () => import('pages/PlaylistPage.vue') }
+    ]
   },
 
   // Always leave this as last one,
