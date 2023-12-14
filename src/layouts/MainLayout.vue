8e9dbdf5b5a9ecbdd8c481a64ce9a273b2d5a503
Daniel Newman <contact@lifeofdan.me>
2023-12-15 00:17:51 +1000

    Format

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,16 +2,38 @@
   <q-layout view="hHh lpR fFf">
     <q-header class="bg-primary text-white">
       <q-toolbar>
-        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+        <q-btn
+          dense
+          flat
+          round
+          icon="menu"
+          @click="toggleLeftDrawer"
+        />
 
         <q-toolbar-title> party crasher </q-toolbar-title>
       </q-toolbar>
     </q-header>
 
-    <q-drawer v-model="leftDrawerOpen" side="left" behavior="mobile">
-      <EssentialLink title="Login" caption="Change user token" link="/" />
-      <EssentialLink title="Playlists" caption="Choose a playlist" link="/playlists" />
-      <EssentialLink title="Queue" caption="Add tracks to play" link="/queue" />
+    <q-drawer
+      v-model="leftDrawerOpen"
+      side="left"
+      behavior="mobile"
+    >
+      <EssentialLink
+        title="Login"
+        caption="Change user token"
+        link="/"
+      />
+      <EssentialLink
+        title="Playlists"
+        caption="Choose a playlist"
+        link="/playlists"
+      />
+      <EssentialLink
+        title="Queue"
+        caption="Add tracks to play"
+        link="/queue"
+      />
     </q-drawer>
 
     <q-page-container>
