7ffa544bfc9e51f2b354852e867a211a56c3e326
Jibao Mansaray <jibaomansaray@gmail.com>
2023-12-21 15:43:14 +1000

    Add `login token` capability
    The user can now login using a six character code

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -6,12 +6,25 @@ export const useAuthStore = defineStore('auth', () => {
   const me = ref<IGetMeData | null>(null)
 
   const actions = {
-    fetchMe: async (): Promise<IGetMeData | null> => {
+    async authenticate (loginToken: string, login: string): Promise<IGetMeData | string> {
+      const response = (loginToken) ? await api.loginTokenAuth(loginToken) : await api.getMe()
+
+      localStorage.setItem('pc_token', response.data?.api_token || login)
+
+      if (response.success) {
+        me.value = response.data
+      }
+
+      return me.value || response.message
+    },
+    fetchMe: async (): Promise<IGetMeData | string> => {
       const response = await api.getMe()
 
-      me.value = response.data ?? null
+      if (response.success) {
+        me.value = response.data
+      }
 
-      return me.value
+      return me.value || response.message
     },
 
     clearMe: () => {
