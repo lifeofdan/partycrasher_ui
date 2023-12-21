2ee5967c2a3f855e276578dc471ea3ddacbaa591
= <jibaomansaray@gmail.com>
2023-12-21 23:10:50 +1000

    Add playlist entity client

---
+++ src/api/entity_api/playlist.ts
@@ -0,0 +1,37 @@
+import { BaseEntity, PaginatorClient } from '.'
+import { IResponse } from '../client'
+
+export interface PlaylistEntity {
+  id: string
+  name: string
+  is_default: boolean
+  description: string
+}
+
+export interface PlaylistTrackEntity {
+  track_id: string
+  playlist_id: string
+  metadata: string
+}
+
+class PlaylistEntityClient extends BaseEntity {
+  constructor (version = 'v1') {
+    super(`/api/${version}/playlists`)
+  }
+
+  public async byId (id: string): Promise<IResponse<PlaylistEntity>> {
+    return await this.doGet(`/${id}`)
+  }
+
+  public all (): PaginatorClient<PlaylistEntity> {
+    return this.paginate()
+  }
+
+  public async addTracks (entries: Array<{ track_id: string, playlist_id: string }>): Promise<IResponse<PlaylistTrackEntity[]>> {
+    return await this.doPost('/add-tracks', entries)
+  }
+}
+
+export function makePlaylistClient (): PlaylistEntityClient {
+  return new PlaylistEntityClient()
+}
