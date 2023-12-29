14fdb39b4b4a4b12b6e0afbcf755ccf36a2ba3d0
Jibao Mansaray <jibaomansaray@gmail.com>
2023-12-30 03:07:47 +1000

    Change state to current mode

--- src/components/MusicRepeatBtn.vue
+++ src/components/MusicRepeatBtn.vue
@@ -17,7 +17,7 @@ import { watch, ref } from 'vue'
 const stateIcon = ref('repeat')
 const musicPlayerStore = useMusicPlayerStore2()
 
-let state = 0
+let currentMode = 0
 const icons = [
   'repeat', 'repeat_one_on', 'repeat_on']
 const mode: RepeatMode[] = ['off', 'one', 'all']
@@ -27,8 +27,8 @@ watch(() => musicPlayerStore.repeatMode, (m: RepeatMode) => {
 })
 
 function repeat () {
-  state++
-  state = state % icons.length
-  stateIcon.value = icons[state]
+  currentMode++
+  currentMode = currentMode % mode.length
+  musicPlayerStore.repeat(mode[currentMode])
 }
 </script>
