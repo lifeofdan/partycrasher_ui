20c29e8fa499de3e6943a7016afd3f9b9a39b900
Jibao Mansaray <jibaomansaray@gmail.com>
2023-12-29 16:48:04 +1000

    Return a function for the entire store

--- src/stores/musicPlayer2.ts
+++ src/stores/musicPlayer2.ts
@@ -1,6 +1,6 @@
 import { defineStore } from 'pinia'
 import { TrackEntity, makeTrackClient } from 'src/api/entity_api/track'
-import { reactive } from 'vue'
+import { computed, reactive } from 'vue'
 
 export type RepeatMode = 'off' | 'one' | 'all'
 
@@ -36,190 +36,210 @@ function makeDefaultPlaying (): AudioCurrentlyPlayling {
   }
 }
 
-export const useMusicPlayerStore2 = defineStore('musicPlayer2', {
-  state (): { _player: HTMLAudioElement, _playing: AudioCurrentlyPlayling } {
-    const player = new Audio()
-    const playing = reactive(makeDefaultPlaying())
-    const playTrack = this.actions?.playTrack
-    const playNext = this.actions?.playNext
+export const useMusicPlayerStore2 = defineStore('musicPlayer2', () => {
+  const player = new Audio()
+  const playing = reactive(makeDefaultPlaying())
 
-    player.addEventListener('timeupdate', () => {
-      playing.at = player.currentTime
-      playing.state = 'playing'
-      playing.progress = (playing.at < 0) ? 0 : (playing.at / playing.duration) * 100
-    })
+  player.addEventListener('timeupdate', () => {
+    playing.at = player.currentTime
+    playing.state = 'playing'
+    playing.progress = (playing.at < 0) ? 0 : (playing.at / playing.duration) * 100
+  })
 
-    player.addEventListener('durationchange', () => {
-      playing.duration = player.duration
-      playing.progress = 0
-    })
+  player.addEventListener('durationchange', () => {
+    playing.duration = player.duration
+    playing.progress = 0
+  })
 
-    player.addEventListener('playing', () => {
-      playing.state = 'playing'
-    })
+  player.addEventListener('playing', () => {
+    playing.state = 'playing'
+  })
 
-    player.addEventListener('pause', () => {
-      playing.state = 'pause'
-    })
+  player.addEventListener('pause', () => {
+    playing.state = 'pause'
+  })
 
-    player.addEventListener('ended', () => {
-      if (playing.shuffle && playing.tracks.length > 0 && playTrack !== undefined) {
-        let nextId = ''
-        do {
-          nextId = playing.tracks[Math.floor(Math.random() * playing.tracks.length - 1)].id
-        } while (nextId === playing.track_id)
-        void playTrack(nextId)
-        return
-      }
-
-      if (playing.repeat === 'one' && playTrack !== undefined) {
-        void playTrack(playing.track_id)
-        return
-      }
-
-      if (playing.repeat === 'all' && playNext !== undefined) {
-        void playNext()
-        return
-      }
-
-      playing.state = 'ended'
-    })
-
-    return {
-      _player: player,
-      _playing: playing
+  player.addEventListener('ended', () => {
+    if (playing.shuffle && playing.tracks.length > 0 && playTrack !== undefined) {
+      let nextId = ''
+      do {
+        nextId = playing.tracks[Math.floor(Math.random() * playing.tracks.length - 1)].id
+      } while (nextId === playing.track_id)
+      void playTrack(nextId)
+      return
     }
-  },
-  getters: {
-    oneOf: (store) => (trackId: string, playlistId: string, albumId: string) => {
-      if (trackId.length > 0 && trackId === store._playing.track_id) {
-        return true
-      }
-      if (trackId !== store._playing.track_id) {
-        return false
-      }
 
-      if ((playlistId.length > 0 && playlistId === store._playing.playlist_id) || (albumId.length > 0 && albumId === store._playing.album_id)) {
-        return true
-      }
+    if (playing.repeat === 'one' && playTrack !== undefined) {
+      void playTrack(playing.track_id)
+      return
+    }
+
+    if (playing.repeat === 'all' && playNext !== undefined) {
+      void playNext()
+      return
+    }
+
+    playing.state = 'ended'
+  })
+
+  const progress = computed(() => playing.progress)
+  const state = computed(() => playing.state)
+  const trackPlayling = computed(() => playing.track_id)
+  const playlistPlayling = computed(() => playing.playlist_id)
+  const albumPlayling = computed(() => playing.album_id)
+  const shuffling = computed(() => playing.shuffle)
+  const repeatMode = computed(() => playing.repeat)
+  const oneOf = computed(() => (trackId: string, playlistId: string, albumId: string) => {
+    if (trackId.length > 0 && trackId === playing.track_id) {
+      return true
+    }
+    if (trackId !== playing.track_id) {
       return false
-    },
-    progress: (store) => store._playing.progress,
-    state: (store) => store._playing.state,
-    trackPlayling: (store) => store._playing.track_id,
-    playlistPlayling: (store) => store._playing.playlist_id,
-    albumPlayling: (store) => store._playing.album_id,
-    shuffling: (store) => store._playing.shuffle,
-    repeatMode: (store) => store._playing.repeat
-  },
-  actions: {
-    stop () {
-      if (this._playing.state === 'playing') {
-        this._player.pause()
-        this._player.currentTime = 0
-        this._player.src = ''
-        this._playing.state = 'stop'
-      }
-    },
-    pause () {
-      if (this._playing.state === 'playing') {
-        this._player.pause()
-      }
-    },
-    async resume () {
-      if (this._playing.state === 'pause') {
-        await this._player.play()
-      }
-    },
-    toggleShuffle () {
-      this._playing.shuffle = !this._playing.shuffle
-    },
-    repeat (mode: RepeatMode) {
-      this._playing.repeat = mode
-    },
-    async togglePlay () {
-      if (this._playing.track_id !== null) {
-        if (this._playing.state === 'playing') {
-          this._player.pause()
-        } else {
-          await this._player.play()
-        }
-      }
-    },
-    async playTrack (id: string, playlistId = '', albumId = ''): Promise<void> {
-      if (playlistId.length > 0) {
-        return await this.playPlaylist(playlistId, id)
-      }
+    }
 
-      if (albumId.length > 0) {
-        return await this.playAlbum(albumId, id)
-      }
+    if ((playlistId.length > 0 && playlistId === playing.playlist_id) || (albumId.length > 0 && albumId === playing.album_id)) {
+      return true
+    }
+    return false
+  })
 
-      this._playing.track_id = ''
-      this._player.pause()
-      this._player.currentTime = 0
-      this._player.src = await trackClient.stream(id)
-      this._playing.track_id = id
-      await this._player.play()
-    },
-    async playPlaylist (id: string, trackId = ''): Promise<void> {
-      const response = await trackClient.byPlaylist(id)
-      if (response.success) {
-        this._playing.playlist_id = id
-        this._playing.album_id = ''
-        this._playing.tracks = response.data ?? []
-        if (this._playing.tracks.length > 0) {
-          if (trackId.length > 0) {
-            this._playing.tracks.find((track, index) => {
-              if (track.id === trackId) {
-                this._playing.track_index = index
-                return true
-              }
-              return false
-            })
-          } else {
-            this._playing.track_index = 0
-          }
-          await this.playTrack(this._playing.tracks[this._playing.track_index].id)
-        }
-      }
-    },
-    async playAlbum (id: string, trackId = ''): Promise<void> {
-      const response = await trackClient.byAlbum(id)
-      if (response.success) {
-        this._playing.album_id = id
-        this._playing.playlist_id = ''
-        this._playing.tracks = response.data ?? []
-        if (this._playing.tracks.length > 0) {
-          if (trackId.length > 0) {
-            this._playing.tracks.find((track, index) => {
-              if (track.id === trackId) {
-                this._playing.track_index = index
-                return true
-              }
-              return false
-            })
-          } else {
-            this._playing.track_index = 0
-          }
+  function stop (): void {
+    if (playing.state === 'playing') {
+      player.pause()
+      player.currentTime = 0
+      player.src = ''
+      playing.state = 'stop'
+    }
+  }
 
-          await this.playTrack(this._playing.tracks[this._playing.track_index].id)
-        }
-      }
-    },
+  function pause (): void {
+    if (playing.state === 'playing') {
+      player.pause()
+    }
+  }
 
-    async playNext (): Promise<void> {
-      if (this._playing.tracks.length > 0) {
-        this._playing.track_index = (this._playing.track_index + 1) % this._playing.tracks.length
-        await this.playTrack(this._playing.tracks[this._playing.track_index].id)
-      }
-    },
-    async playPrevious (): Promise<void> {
-      if (this._playing.tracks.length > 0) {
-        this._playing.track_index--
-        this._playing.track_index = (this._playing.track_index >= 0) ? this._playing.track_index : 0
-        await this.playTrack(this._playing.tracks[this._playing.track_index].id)
+  async function resume (): Promise<void> {
+    if (playing.state === 'pause') {
+      await player.play()
+    }
+  }
+
+  function toggleShuffle (): void {
+    playing.shuffle = !playing.shuffle
+  }
+
+  function repeat (mode: RepeatMode): void {
+    playing.repeat = mode
+  }
+
+  async function togglePlay (): Promise<void> {
+    if (playing.track_id !== null) {
+      if (playing.state === 'playing') {
+        player.pause()
+      } else {
+        await player.play()
       }
     }
+  }
+
+  async function playTrack (id: string, playlistId = '', albumId = ''): Promise<void> {
+    if (playlistId.length > 0) {
+      return await playPlaylist(playlistId, id)
+    }
+
+    if (albumId.length > 0) {
+      return await playAlbum(albumId, id)
+    }
+
+    playing.track_id = ''
+    player.pause()
+    player.currentTime = 0
+    player.src = await trackClient.stream(id)
+    playing.track_id = id
+    await player.play()
+  }
+
+  async function playPlaylist (id: string, trackId = ''): Promise<void> {
+    const response = await trackClient.byPlaylist(id)
+    if (response.success) {
+      playing.playlist_id = id
+      playing.album_id = ''
+      playing.tracks = response.data ?? []
+      if (playing.tracks.length > 0) {
+        if (trackId.length > 0) {
+          playing.tracks.find((track, index) => {
+            if (track.id === trackId) {
+              playing.track_index = index
+              return true
+            }
+            return false
+          })
+        } else {
+          playing.track_index = 0
+        }
+        await playTrack(playing.tracks[playing.track_index].id)
+      }
+    }
+  }
+
+  async function playAlbum (id: string, trackId = ''): Promise<void> {
+    const response = await trackClient.byAlbum(id)
+    if (response.success) {
+      playing.album_id = id
+      playing.playlist_id = ''
+      playing.tracks = response.data ?? []
+      if (playing.tracks.length > 0) {
+        if (trackId.length > 0) {
+          playing.tracks.find((track, index) => {
+            if (track.id === trackId) {
+              playing.track_index = index
+              return true
+            }
+            return false
+          })
+        } else {
+          playing.track_index = 0
+        }
+
+        await playTrack(playing.tracks[playing.track_index].id)
+      }
+    }
+  }
+
+  async function playNext (): Promise<void> {
+    if (playing.tracks.length > 0) {
+      playing.track_index = (playing.track_index + 1) % playing.tracks.length
+      await playTrack(playing.tracks[playing.track_index].id)
+    }
+  }
+
+  async function playPrevious (): Promise<void> {
+    if (playing.tracks.length > 0) {
+      playing.track_index--
+      playing.track_index = (playing.track_index >= 0) ? playing.track_index : 0
+      await playTrack(playing.tracks[playing.track_index].id)
+    }
+  }
+
+  return {
+    progress,
+    state,
+    trackPlayling,
+    playlistPlayling,
+    albumPlayling,
+    shuffling,
+    repeatMode,
+    oneOf,
+    stop,
+    pause,
+    resume,
+    toggleShuffle,
+    repeat,
+    togglePlay,
+    playTrack,
+    playPlaylist,
+    playAlbum,
+    playNext,
+    playPrevious
   }
 })
