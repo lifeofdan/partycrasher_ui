94fd3deec39393f2fbe46a45b0b2c95037cd0fdc
Jibao Mansaray <jibaomansaray@gmail.com>
2024-01-02 22:00:05 +1000

    Return media URL instead
    Since we are now using cookies, we can return the URL
    directly.

--- src/api/entity_api/media.ts
+++ src/api/entity_api/media.ts
@@ -1,16 +1,18 @@
 import { BaseEntity } from '.'
 
 class MediaApiClient extends BaseEntity {
+  private readonly baseUrl: string
   constructor (version = 'v1') {
     super(`/api/${version}`)
+    this.baseUrl = `/api/${version}`
   }
 
   public async byId (id: string): Promise<string> {
-    return URL.createObjectURL(await this.doGetBlob(`/serve/${id}`))
+    return `${this.baseUrl}/serve/${id}`
   }
 
   public async byTrackId (id: string): Promise<string> {
-    return URL.createObjectURL(await this.doGetBlob(`/stream/${id}`))
+    return `${this.baseUrl}/stream/${id}`
   }
 }
 
